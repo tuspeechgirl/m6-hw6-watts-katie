8b307bca8da9fe1904b106c1b80b8a976a048d3c
Katie Watts <kathryn.watts@shipk12.org>
2019-09-29 14:31:26 -0400

    Add JS to make colors change and messages appear

---
+++ main.js
@@ -0,0 +1,64 @@
+//JS for I//
+//Selecting each box//
+var challeng1 = document.querySelector('.card-1');
+var challeng1B = document.querySelector('.card-2');
+var challeng1C = document.querySelector('.card-3');
+//function to change color//
+function bgChange(){
+              changeColor('red');
+                  }
+//function to place the color on each box//
+  function changeColor(color) {
+          document.querySelector('.card-1').style.background = color;
+          document.querySelector('.card-2').style.background = color;
+          document.querySelector('.card-3').style.background = color;
+                        }
+//call the function to happen on hover//
+challeng1.onmouseover=bgChange;
+challeng1B.onmouseover=bgChange;
+challeng1C.onmouseover=bgChange;
+
+
+
+//Js for II//
+
+var challenge2 = document.querySelector('.card-1'); //selects box 1//
+
+var newEl = document.createElement('h6'); //creates new element//
+
+//creates function of adding the node to the box//
+function numberTwo(){
+newEl.appendChild(document.createTextNode('Oooh- close but, no cigar.'));
+challenge2.appendChild(newEl);
+}
+//calls the function to occur onclick//
+challenge2.onclick=numberTwo;
+
+
+//Js for III//
+
+var challenge3 = document.querySelector('.card-2'); //selects box 1//
+
+var newElem = document.createElement('h6'); //creates new element//
+
+//creates function of adding the node to the box//
+function numberThree(){
+newElem.appendChild(document.createTextNode('DING DING DING-We have a winner.'));
+challenge3.appendChild(newElem);
+}
+//calls the function to occur onclick//
+challenge3.onclick=numberThree;
+
+//Js for IV//
+
+var challenge4 = document.querySelector('.card-3'); //selects box 1//
+
+var newElement = document.createElement('h6'); //creates new element//
+
+//creates function of adding the node to the box//
+function numberFour(){
+newElement.appendChild(document.createTextNode('Oops, better luck next time.'));
+challenge4.appendChild(newElement);
+}
+//calls the function to occur onclick//
+challenge4.onclick=numberFour;
